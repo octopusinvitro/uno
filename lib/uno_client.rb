9a36e2697b3ee6033ea06eb4013afd88b36ff942
private <user@example.com>
2016-06-21 13:57:53 +0100

    Add the join method to the Uno client

---
+++ lib/uno_client.rb
@@ -0,0 +1,32 @@
+class UnoClient
+
+  attr_reader :name, :client, :base_url
+
+  def initialize name, client, base_url
+    @name     = name
+    @client   = client
+    @base_url = base_url
+  end
+
+  def join_game
+    response = client.post(
+      base_url + '/join',
+      {
+        data:   {name: name}.to_json,
+        accept: :json
+      }
+    )
+    JSON.parse(response, {symbolize_names: true})
+  end
+  #
+  # def get_cards
+  #   response = RestClient.get 'http://localhost:8080/cards', {:params => {:name => @name}}
+  #   puts response
+  # end
+  #
+  # def deal
+  #   response = RestClient.post 'http://localhost:8080/deal', :data =>{}.to_json, :accept => :json
+  #   puts response
+  # end
+
+end
--- spec/uno_client_spec.rb
+++ spec/uno_client_spec.rb
@@ -1,5 +1,33 @@
 describe "UnoClient" do
 
-  let(:uno) {UnoClient.new}
+  let(:client) {double(RestClient)}
+  let(:url)    {"http://localhost:8080"}
+  let(:uno)    {UnoClient.new("Jon", client, url)}
+
+  describe "when joining the game" do
+
+    before(:each) do
+      allow(client).to receive(:post).and_return('{"response_key": "response_value"}')
+    end
+
+    it "sends a post request to join the game" do
+      uno.join_game
+      expect(client).to have_received(:post).once
+    end
+
+    it "sends a post request with the right parameters" do
+      params = {
+        data: '{"name":"Jon"}',
+        accept: :json
+      }
+      uno.join_game
+      expect(client).to have_received(:post).with(url + "/join", params)
+    end
+
+    it "returns the right response when joined" do
+      expect(uno.join_game).to eq( {response_key: "response_value"} )
+    end
+
+  end
 
 end
