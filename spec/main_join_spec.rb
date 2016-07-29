f2c44975636e97099ff35a60ea16b2340fd53d4f
private <user@example.com>
2016-07-29 19:56:21 +0100

    Split the tests for join into their own file

---
+++ spec/main_join_spec.rb
@@ -0,0 +1,70 @@
+describe "Main" do
+
+  let(:uno) {UnoServer.new}
+  let(:app) {Main.new(MainHelper.new(uno))}
+
+  describe "when a player joins the game" do
+    describe "(JSON)" do
+      before(:each) do
+        header "Accept", "application/json"
+      end
+
+      it "welcomes the player" do
+        post "/join", {"name" => "Jane"}
+        expect_to_have_joined(true, Messages::JOIN_SUCCESS)
+      end
+
+      it "sends an error message if no name was sent" do
+        post "/join", {}
+        expect_to_have_joined(false, Messages::JOIN_FAILURE)
+      end
+
+      it "sends an error message if no data was sent" do
+        post "/join"
+        expect_to_have_joined(false, Messages::JOIN_FAILURE)
+      end
+
+      it "sends an error message if player could not join the game" do
+        fill_the_game
+        post "/join", {"name" => "Jane"}
+        expect_to_have_joined(false, Messages::JOIN_FAILURE)
+      end
+    end
+
+    describe "(views)" do
+      before(:each) do
+        header "Accept", "text/html"
+      end
+
+      it "renders the join view if join was successful" do
+        post "/join", {"name" => "Jane"}
+        expect_body_to_include(Messages::JOIN_TITLE, Messages::JOIN_SUCCESS)
+        expect(last_response.body).to include("a maximum of 4 players")
+      end
+
+      it "renders the players" do
+        post "/join", {"name" => "Jane"}
+        post "/join", {"name" => "Joe"}
+        expect(last_response.body).to include("Jane")
+        expect(last_response.body).to include("Joe (you)")
+      end
+
+      it "renders the index page page if no name was sent" do
+        post "/join", {"name" => ""}
+        expect_body_to_include(Messages::MAIN_TITLE, Messages::JOIN_FAILURE)
+      end
+
+      it "renders the index page page if game is full" do
+        fill_the_game
+        post "/join", {"name" => "Jane"}
+        expect_body_to_include(Messages::MAIN_TITLE, Messages::JOIN_FAILURE)
+      end
+    end
+
+    # it "returns a 406 error if not acceptable MIME type" do
+    #   header "Accept", "text/xml"
+    #   post "/join", {"name" => "Jane"}
+    #   expect(last_response.status).to eq(406)
+    # end
+  end
+end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -15,66 +15,6 @@ describe "Main" do
     expect(last_response.body).to include(Messages::MAIN_TITLE)
   end
 
-  describe "when a player joins the game" do
-    describe "(JSON)" do
-      before(:each) do
-        header "Accept", "application/json"
-      end
-
-      it "welcomes the player" do
-        post "/join", {"name" => "Jane"}
-        expect_to_have_joined(true, Messages::JOIN_SUCCESS)
-      end
-
-      it "sends an error message if no name was sent" do
-        post "/join", {}
-        expect_to_have_joined(false, Messages::JOIN_FAILURE)
-      end
-
-      it "sends an error message if no data was sent" do
-        post "/join"
-        expect_to_have_joined(false, Messages::JOIN_FAILURE)
-      end
-
-      it "sends an error message if player could not join the game" do
-        fill_the_game
-        post "/join", {"name" => "Jane"}
-        expect_to_have_joined(false, Messages::JOIN_FAILURE)
-      end
-    end
-
-    describe "(views)" do
-      before(:each) do
-        header "Accept", "text/html"
-      end
-
-      it "renders the join view if join was successful" do
-        post "/join", {"name" => "Jane"}
-        expect_body_to_include(Messages::JOIN_TITLE, Messages::JOIN_SUCCESS)
-        expect(last_response.body).to include("a maximum of 4 players")
-      end
-
-      it "renders the players" do
-        post "/join", {"name" => "Jane"}
-        post "/join", {"name" => "Joe"}
-        expect(last_response.body).to include("Jane")
-        expect(last_response.body).to include("Joe (you)")
-      end
-
-      it "renders the index page page if no name was sent" do
-        post "/join", {"name" => ""}
-        expect_body_to_include(Messages::MAIN_TITLE, Messages::JOIN_FAILURE)
-      end
-
-      it "renders the index page page if game is full" do
-        fill_the_game
-        post "/join", {"name" => "Jane"}
-        expect_body_to_include(Messages::MAIN_TITLE, Messages::JOIN_FAILURE)
-      end
-    end
-
-  end
-
   describe "when it's deal time" do
     describe "(JSON)" do
       before(:each) do
@@ -162,30 +102,5 @@ describe "Main" do
 
   end
 
-  def expect_to_have_joined(joined, status)
-    response = JSON.parse(last_response.body, symbolize_names: true)
-    expect(last_response).to be_ok
-    expect(response[:joined]).to be(joined)
-    expect(response[:status]).to eq(status)
-  end
 
-  def expect_body_to_include(title, status)
-    expect(last_response).to be_ok
-    expect(last_response.body).to include("<title>#{title}</title>")
-    expect(last_response.body).to include("<h2>#{title}</h2>")
-    expect(last_response.body).to include(status)
-  end
-
-  def expect_to_have_dealt(dealt, status)
-    response = JSON.parse(last_response.body, symbolize_names: true)
-    expect(last_response).to be_ok
-    expect(response[:dealt]).to be(dealt)
-    expect(response[:status]).to eq(status)
-  end
-
-  def expect_response_to_eq(expected)
-    response = JSON.parse(last_response.body, symbolize_names: true)
-    expect(last_response).to be_ok
-    expect(response).to eq(expected)
-  end
 end
