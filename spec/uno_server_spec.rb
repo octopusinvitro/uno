d10cde9e86b31f8827de45de97f1c792aa780bf3
private <user@example.com>
2016-08-21 20:05:25 +0100

    Rename the Uno server to server in tests

--- spec/uno_server_spec.rb
+++ spec/uno_server_spec.rb
@@ -1,75 +1,75 @@
 describe "UnoServer" do
 
-  let(:uno)       {@uno}
-  let(:max_cards) {uno.max_cards}
+  let(:server)       {@server}
+  let(:max_cards) {server.max_cards}
 
   before(:each) do
-    @uno = UnoServer.new(Uno.new)
+    @server = UnoServer.new(Uno.new)
   end
 
   it "starts with a deck of 108 cards" do
-    expect(uno.deck.size).to eq(108)
+    expect(server.deck.size).to eq(108)
   end
 
   it "allows a player to join the game" do
-    uno.join_game?("Jane")
-    expect(uno.players.size).to eq(1)
-    expect(uno.players.first[:name]).to eq("Jane")
+    server.join_game?("Jane")
+    expect(server.players.size).to eq(1)
+    expect(server.players.first[:name]).to eq("Jane")
   end
 
   it "doesn't allow a player to join if there are four already" do
-    expect(uno.join_game?("Jane")).to be(true)
-    expect(uno.join_game?("Joe") ).to be(true)
-    expect(uno.join_game?("Moe") ).to be(true)
-    expect(uno.join_game?("May") ).to be(true)
-    expect(uno.join_game?("Won't join")).to be(false)
+    expect(server.join_game?("Jane")).to be(true)
+    expect(server.join_game?("Joe") ).to be(true)
+    expect(server.join_game?("Moe") ).to be(true)
+    expect(server.join_game?("May") ).to be(true)
+    expect(server.join_game?("Won't join")).to be(false)
   end
 
   it "doesn't deal cards if there are no players" do
-    expect(uno.deal?).to be(false)
+    expect(server.deal?).to be(false)
   end
 
   it "deals cards if there are players" do
-    uno.join_game?("Jane")
-    expect(uno.deal?).to be(true)
+    server.join_game?("Jane")
+    expect(server.deal?).to be(true)
   end
 
   it "delivers the maximum cards to each player" do
-    uno.join_game?("Jane")
-    uno.deal?
-    expect(uno.players.first[:cards].size).to eq(max_cards)
+    server.join_game?("Jane")
+    server.deal?
+    expect(server.players.first[:cards].size).to eq(max_cards)
   end
 
   it "shuffles the cards" do
-    uno.join_game?("Jane")
-    uno.deal?
-    expect(uno.players.first[:cards]).not_to eq(uno.deck[0...max_cards])
+    server.join_game?("Jane")
+    server.deal?
+    expect(server.players.first[:cards]).not_to eq(server.deck[0...max_cards])
   end
 
   it "sees the cards of a player" do
-    uno.join_game?("Jane")
-    uno.deal?
-    expect(uno.see_cards_of("Jane")).to eq(uno.players.first[:cards])
+    server.join_game?("Jane")
+    server.deal?
+    expect(server.see_cards_of("Jane")).to eq(server.players.first[:cards])
   end
 
   it "can't see the cards of non-existent player" do
-    uno.join_game?("Jane")
-    uno.deal?
-    expect(uno.see_cards_of("Joe")).to eq([])
+    server.join_game?("Jane")
+    server.deal?
+    expect(server.see_cards_of("Joe")).to eq([])
   end
 
   it "flips top card of the deck" do
-    top_card = uno.pool.last
-    uno.flip_top_card
-    expect(uno.pool.first).to eq(top_card)
+    top_card = server.pool.last
+    server.flip_top_card
+    expect(server.pool.first).to eq(top_card)
   end
 
   it "resets the game" do
-    uno.join_game?("Jane")
-    uno.deal?
-    uno.reset
-    expect(uno.pool).to eq(uno.deck)
-    expect(uno.players).to eq([])
+    server.join_game?("Jane")
+    server.deal?
+    server.reset
+    expect(server.pool).to eq(server.deck)
+    expect(server.players).to eq([])
   end
 
   it "plays a player's turn" do
