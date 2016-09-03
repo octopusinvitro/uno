a252fdd5b3084be4a6a2c76463e3ed2a3247dd6d
private <user@example.com>
2016-09-03 04:36:20 +0100

    Add freeze to messages and get rid of method

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,17 +1,14 @@
 module Messages
-  NOT_FOUND     = "Page Not Found"
-  JOIN_SUCCESS  = "Welcome!"
-  DEAL_SUCCESS  = "Cards were dealt!"
-  CARDS_SUCCESS = "Success"
-  JOIN_FAILURE  = "Sorry - missing name or game not accepting new players"
-  DEAL_FAILURE  = "Sorry - there are no players to deal to"
-  CARDS_FAILURE = "Sorry - either you are not part of the game or you have no cards"
-  MAIN_TITLE    = "UNO :: Play online"
-  JOIN_TITLE    = "UNO :: Join the game"
-  DEAL_TITLE    = "UNO :: Deal cards"
-  CARDS_TITLE   = "UNO :: Get cards"
-
-  def self.max_players_info(max_players)
-    "Remember that there is a maximum of #{max_players} players."
-  end
+  NOT_FOUND     = "Page Not Found".freeze
+  JOIN_SUCCESS  = "Welcome!".freeze
+  DEAL_SUCCESS  = "Cards were dealt!".freeze
+  CARDS_SUCCESS = "Success".freeze
+  JOIN_FAILURE  = "Sorry - missing name or game not accepting new players".freeze
+  DEAL_FAILURE  = "Sorry - there are no players to deal to".freeze
+  CARDS_FAILURE = "Sorry - either you are not part of the game or you have no cards".freeze
+  MAIN_TITLE    = "UNO :: Play online".freeze
+  JOIN_TITLE    = "UNO :: Join the game".freeze
+  DEAL_TITLE    = "UNO :: Deal cards".freeze
+  CARDS_TITLE   = "UNO :: Get cards".freeze
+  MAX_PLAYERS_INFO = "Remember that there is a maximum of #{Constants::MAX_PLAYERS} players.".freeze
 end
--- spec/messages_spec.rb
+++
@@ -1,5 +0,0 @@
-describe "Messages" do
-  it "prints the maximum number of players" do
-    expect(Messages.max_players_info(4)).to include("4")
-  end
-end
