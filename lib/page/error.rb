0f9ceef84d522876a4d3f1267c5962ba87d74058
private <user@example.com>
2016-09-04 13:21:56 +0100

    Add an error page class

---
+++ lib/page/error.rb
@@ -0,0 +1,7 @@
+module Page
+  class Error
+    def title
+      Messages::NOT_FOUND
+    end
+  end
+end
---
+++ spec/page/error_spec.rb
@@ -0,0 +1,7 @@
+describe "Page::Error" do
+  let(:page) { Page::Error.new }
+
+  it "has a title" do
+    expect(page.title).to eq(Messages::NOT_FOUND)
+  end
+end
