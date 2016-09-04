f4aa53d5325ff8d4700167e47c9fc30136f036dd
private <user@example.com>
2016-09-04 14:06:51 +0100

    Use pass in routes to remove logic

--- lib/main.rb
+++ lib/main.rb
@@ -23,50 +23,64 @@ class Main < Sinatra::Base
     erb :index
   end
 
-  post "/join", :provides => ["html", "json"] do
+  post "/join", :provides => ["json"] do
+    pass unless request.accept.first.to_s == "application/json"
     response = helper.response_for_join(params)
-    request.accept.each do |type|
-      case type.to_s
-      when "text/html"
-        if response[:joined]
-          @page = Page::Join.new(response: response, params: params)
-          halt erb :join
-        else
-          status = "<p class=\"status\">#{Messages::JOIN_FAILURE}</p>"
-          @page = Page::Index.new(status: status)
-          halt erb :index
-        end
-      when "application/json"
-        halt response.to_json
-      end
-    end
+    response.to_json
   end
 
-  get "/deal", :provides => ["html", "json"] do
+  post "/join", :provides => ["html"] do
+    pass unless request.accept.first.to_s == "text/html"
+    response = helper.response_for_join(params)
+    pass unless response[:joined]
+    @page = Page::Join.new(response: response, params: params)
+    erb :join
+  end
+
+  post "/join", :provides => ["html"] do
+    pass unless request.accept.first.to_s == "text/html"
+    response = helper.response_for_join(params)
+    pass unless !response[:joined]
+    status = "<p class=\"status\">#{Messages::JOIN_FAILURE}</p>"
+    @page = Page::Index.new(status: status)
+    erb :index
+  end
+
+  post "/join" do
+    error 406
+  end
+
+  get "/deal", :provides => ["json"] do
+    pass unless request.accept.first.to_s == "application/json"
     response = helper.response_for_deal
-    request.accept.each do |type|
-      case type.to_s
-      when "text/html"
-        @page = Page::Deal.new(response: response)
-        halt erb :deal
-      when "application/json"
-        halt response.to_json
-      end
-    end
+    response.to_json
+  end
+
+  get "/deal", :provides => ["html"] do
+    pass unless request.accept.first.to_s == "text/html"
+    response = helper.response_for_deal
+    @page = Page::Deal.new(response: response)
+    erb :deal
+  end
+
+  get "/deal" do
+    error 406
+  end
+
+  get "/cards", :provides => ["json"] do
+    pass unless request.accept.first.to_s == "application/json"
+    response = helper.response_for_cards(params)
+    response.to_json
+  end
+
+  get "/cards", :provides => ["html"] do
+    pass unless request.accept.first.to_s == "text/html"
+    response = helper.response_for_cards(params)
+    @page = Page::Cards.new(response: response)
+    erb :cards
   end
 
   get "/cards" do
-    response = helper.response_for_cards(params)
-    request.accept.each do |type|
-      case type.to_s
-      when "text/html"
-        @page = Page::Cards.new(response: response)
-        halt erb :cards
-      when "text/json"
-        halt response.to_json
-      end
-    end
-
     error 406
   end
 end
